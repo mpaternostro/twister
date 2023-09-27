73827fdf4450f770e6ba3c46bd22be308edc3808
Marcelo <marcelodpaternostro@gmail.com>
2023-09-27 11:33:54 -0300

    feat: db schema types

--- other/supabase-image-loader.js
+++ other/supabase-image-loader.js
@@ -1,4 +1,6 @@
-export default function supabaseLoader({ src, width, quality }) {
+export default function supabaseLoader(
+  /** @type {import('next/image').ImageLoaderProps} */ { src, width, quality },
+) {
   return `${
     process.env.NEXT_PUBLIC_SUPABASE_URL
   }/storage/v1/render/image/public/${src}?width=${width}&quality=${
